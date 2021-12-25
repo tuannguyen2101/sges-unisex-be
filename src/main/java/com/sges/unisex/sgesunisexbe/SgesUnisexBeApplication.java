e689d137ee6f03afb8eab5ff7dcfff92a9262ae9
TuanNguyen21 <nguyentuan21012001@gmail.com>
2021-12-25 11:49:52 +0700

    first commit

---
+++ src/main/java/com/sges/unisex/sgesunisexbe/SgesUnisexBeApplication.java
@@ -0,0 +1,13 @@
+package com.sges.unisex.sgesunisexbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgesUnisexBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgesUnisexBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sges/unisex/sgesunisexbe/SgesUnisexBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sges.unisex.sgesunisexbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgesUnisexBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
